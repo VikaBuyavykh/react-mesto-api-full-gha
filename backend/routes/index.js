5d1a1f223f8bfb0fc1954f901d4c7f899b806383
Vika Buyavykh <vikabuyavykh@gmail.com>
2023-12-03 12:07:36 +0300

    Add crash-test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,6 +9,11 @@ const NotFoundError = require('../errors/not-found-err');
 
 const router = Router();
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
